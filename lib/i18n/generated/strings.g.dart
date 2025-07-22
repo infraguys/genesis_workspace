9696ee6604595504967ad1fe25cba970185113d4
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-07-22 22:14:05 +0800

    clean refactor

--- lib/i18n/generated/strings.g.dart
+++ lib/i18n/generated/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 10 (5 per locale)
 ///
-/// Built on 2025-07-22 at 08:31 UTC
+/// Built on 2025-07-22 at 13:56 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
