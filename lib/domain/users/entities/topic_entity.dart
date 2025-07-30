500d0d2a89a9e657b707c7a16be26344f7b0b0d4
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-07-30 10:26:46 +0300

    layouts, bug fixes

--- lib/domain/users/entities/topic_entity.dart
+++ lib/domain/users/entities/topic_entity.dart
@@ -1,7 +1,12 @@
-class TopicEntity {
+import 'package:equatable/equatable.dart';
+
+class TopicEntity extends Equatable {
   final int maxId;
   final String name;
   Set<int> unreadMessages;
 
   TopicEntity({required this.maxId, required this.name, required this.unreadMessages});
+
+  @override
+  List<Object> get props => [name];
 }
