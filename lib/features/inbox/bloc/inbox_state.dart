4313ae9c6cc4bed98d25d28627010d53d4bce58c
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-08-04 13:54:43 +0300

    inbox streams, read event, messages event

--- lib/features/inbox/bloc/inbox_state.dart
+++ lib/features/inbox/bloc/inbox_state.dart
@@ -1,25 +1,18 @@
 part of 'inbox_cubit.dart';
 
 class InboxState {
-  // List<MessageEntity> dmMessages;
-  List<MessageEntity> channelMessages;
+  Map<String, Map<String, List<MessageEntity>>> channelMessages;
   Map<String, List<MessageEntity>> dmMessages;
 
-  InboxState({
-    required this.dmMessages,
-    required this.channelMessages,
-    // required this.dmMessagesCount,
-  });
+  InboxState({required this.dmMessages, required this.channelMessages});
 
   InboxState copyWith({
-    // List<MessageEntity>? dmMessages,
-    List<MessageEntity>? channelMessages,
+    Map<String, Map<String, List<MessageEntity>>>? channelMessages,
     Map<String, List<MessageEntity>>? dmMessages,
   }) {
     return InboxState(
       dmMessages: dmMessages ?? this.dmMessages,
       channelMessages: channelMessages ?? this.channelMessages,
-      // dmMessagesCount: dmMessagesCount ?? this.dmMessagesCount,
     );
   }
 }
