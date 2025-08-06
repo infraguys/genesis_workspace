854f7c80d40fbfd66e35a92cc24fb0da2f1bbec0
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-08-06 14:52:41 +0300

    sort pubspec

--- lib/features/inbox/bloc/inbox_state.dart
+++ lib/features/inbox/bloc/inbox_state.dart
@@ -3,16 +3,19 @@ part of 'inbox_cubit.dart';
 class InboxState {
   Map<String, Map<String, List<MessageEntity>>> channelMessages;
   Map<String, List<MessageEntity>> dmMessages;
+  int? pendingId;
 
-  InboxState({required this.dmMessages, required this.channelMessages});
+  InboxState({required this.dmMessages, required this.channelMessages, this.pendingId});
 
   InboxState copyWith({
     Map<String, Map<String, List<MessageEntity>>>? channelMessages,
     Map<String, List<MessageEntity>>? dmMessages,
+    int? pendingId,
   }) {
     return InboxState(
       dmMessages: dmMessages ?? this.dmMessages,
       channelMessages: channelMessages ?? this.channelMessages,
+      pendingId: pendingId ?? this.pendingId,
     );
   }
 }
