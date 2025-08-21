56682cef183f011487cf97da0739834c6b9f0426
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-08-21 17:56:43 +0300

    dm navigation

--- lib/core/config/screen_size.dart
+++ lib/core/config/screen_size.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 ScreenSize currentSize(BuildContext context) {
   final width = MediaQuery.sizeOf(context).width;
+  print(width);
   switch (width) {
     case (> 1440):
       return ScreenSize.desktop;
