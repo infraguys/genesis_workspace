fbbb810cd71ea9f1630d6c258eea5a8470bb220c
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-08-28 12:06:26 +0300

    changes

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 110 (55 per locale)
 ///
-/// Built on 2025-08-26 at 11:55 UTC
+/// Built on 2025-08-28 at 08:18 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
