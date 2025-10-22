2f971c4b9f6f22d4511053605fd4ba74e80179f8
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-10-22 11:33:37 +0300

    remove dist

--- lib/i18n/generated/strings.g.dart
+++ lib/i18n/generated/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 256 (128 per locale)
 ///
-/// Built on 2025-10-22 at 08:29 UTC
+/// Built on 2025-10-22 at 08:33 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
