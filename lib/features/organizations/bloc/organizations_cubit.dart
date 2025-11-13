ee869448b0159142ffb2170f11d1b74d2e6167f5
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-11-13 12:15:18 +0300

    remove messages from org items

--- lib/features/organizations/bloc/organizations_cubit.dart
+++ lib/features/organizations/bloc/organizations_cubit.dart
@@ -3,12 +3,15 @@ import 'dart:developer';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis_workspace/core/config/constants.dart';
+import 'package:genesis_workspace/core/enums/message_flag.dart';
+import 'package:genesis_workspace/core/enums/update_message_flags_op.dart';
 import 'package:genesis_workspace/domain/organizations/entities/organization_entity.dart';
 import 'package:genesis_workspace/domain/organizations/usecases/add_organization_use_case.dart';
 import 'package:genesis_workspace/domain/organizations/usecases/get_organization_settings_use_case.dart';
 import 'package:genesis_workspace/domain/organizations/usecases/remove_organization_use_case.dart';
 import 'package:genesis_workspace/domain/organizations/usecases/watch_organizations_use_case.dart';
 import 'package:genesis_workspace/domain/real_time_events/entities/event/message_event_entity.dart';
+import 'package:genesis_workspace/domain/real_time_events/entities/event/update_message_flags_event_entity.dart';
 import 'package:genesis_workspace/features/authentication/domain/entities/server_settings_entity.dart';
 import 'package:genesis_workspace/services/organizations/organization_switcher_service.dart';
 import 'package:genesis_workspace/services/real_time/multi_polling_service.dart';
@@ -36,6 +39,7 @@ class OrganizationsCubit extends Cubit<OrganizationsState> {
       onError: (error, _) => inspect(error),
     );
     _messagesEventsSubscription = _multiPollingService.messageEventsStream.listen(_onMessageEvents);
+    _messagesFlagsEventsSubscription = _multiPollingService.messageFlagsEventsStream.listen(_onMessageFlagsEvents);
   }
 
   final WatchOrganizationsUseCase _watchOrganizationsUseCase;
@@ -48,6 +52,7 @@ class OrganizationsCubit extends Cubit<OrganizationsState> {
   late final StreamSubscription<List<OrganizationEntity>> _organizationsSubscription;
 
   late final StreamSubscription<MessageEventEntity> _messagesEventsSubscription;
+  late final StreamSubscription<UpdateMessageFlagsEventEntity> _messagesFlagsEventsSubscription;
 
   void _onOrganizationsUpdated(List<OrganizationEntity> organizations) {
     final int? persistedSelection = state.selectedOrganizationId ?? AppConstants.selectedOrganizationId;
@@ -105,7 +110,6 @@ class OrganizationsCubit extends Cubit<OrganizationsState> {
 
   void _onMessageEvents(MessageEventEntity event) {
     final orgId = event.organizationId;
-    inspect(event);
     List<OrganizationEntity> updatedOrganizations = [...state.organizations];
     final org = updatedOrganizations.firstWhere((element) => element.id == orgId);
     final index = updatedOrganizations.indexOf(org);
@@ -113,10 +117,21 @@ class OrganizationsCubit extends Cubit<OrganizationsState> {
     emit(state.copyWith(organizations: updatedOrganizations));
   }
 
+  void _onMessageFlagsEvents(UpdateMessageFlagsEventEntity event) {
+    List<OrganizationEntity> updatedOrganizations = [...state.organizations];
+    if (event.flag == MessageFlag.read && event.op == UpdateMessageFlagsOp.add) {
+      updatedOrganizations.forEach((org) {
+        org.unreadMessages.removeAll(event.messages);
+      });
+    }
+    emit(state.copyWith(organizations: updatedOrganizations));
+  }
+
   @override
   Future<void> close() {
     _messagesEventsSubscription.cancel();
     _organizationsSubscription.cancel();
+    _messagesFlagsEventsSubscription.cancel();
     return super.close();
   }
 }
