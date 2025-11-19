b91ed879e5452833c9fd61706c258dacc2025aa7
Eugene Koretsky <E.Koretsky@outlook.com>
2025-11-19 19:16:01 +0300

    refactor: enhance UserAvatar widget with default icons and simplified URL handling

--- lib/core/widgets/user_avatar.dart
+++ lib/core/widgets/user_avatar.dart
@@ -2,30 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:genesis_workspace/core/config/constants.dart';
 
 class UserAvatar extends StatelessWidget {
-  const UserAvatar({super.key, this.avatarUrl, this.size});
+  const UserAvatar({super.key, this.avatarUrl, this.size}) : _defaultIcons = Icons.person;
+
+  const UserAvatar.group({super.key, this.avatarUrl, this.size}) : _defaultIcons = Icons.groups;
 
   final String? avatarUrl;
   final double? size;
 
+  final IconData _defaultIcons;
+
+  double? get radius => size != null ? size! / 2 : null;
+
   @override
   Widget build(BuildContext context) {
     if (avatarUrl != null && avatarUrl!.isNotEmpty) {
-      String fullUrl;
-      if (avatarUrl!.contains("http")) {
-        fullUrl = avatarUrl!;
-      } else {
-        fullUrl = "${AppConstants.baseUrl}$avatarUrl";
-      }
+      var fullUrl = avatarUrl!.startsWith("http") ? avatarUrl! : "${AppConstants.baseUrl}$avatarUrl";
+
       return CircleAvatar(
-        radius: size != null ? size! / 2 : null,
+        radius: radius,
         backgroundImage: NetworkImage(fullUrl),
         backgroundColor: Colors.grey[200],
       );
-    } else {
-      return CircleAvatar(
-        radius: size != null ? size! / 2 : null,
-        child: Icon(Icons.person),
-      );
     }
+
+    return CircleAvatar(
+      radius: radius,
+      child: Icon(_defaultIcons),
+    );
   }
 }
