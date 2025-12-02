e2ee77f17433c8c7660e250544e431654bd41c6e
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-12-02 16:10:20 +0300

    refactor

--- lib/domain/users/entities/subscription_entity.dart
+++ lib/domain/users/entities/subscription_entity.dart
@@ -1,26 +1,26 @@
 import 'package:genesis_workspace/domain/users/entities/channel_entity.dart';
 
 class SubscriptionEntity {
+  final int streamId;
   final String name;
   final List<int>? subscribers;
-  final int streamId;
   final String description;
   final String color;
   final bool isMuted;
 
   SubscriptionEntity({
+    required this.streamId,
     required this.name,
     required this.subscribers,
-    required this.streamId,
     required this.description,
     required this.color,
     required this.isMuted,
   });
 
   factory SubscriptionEntity.fake({
-    String name = '-1',
+    String name = 'Fake channel',
     List<int>? subscribers,
-    int streamId = 0,
+    int streamId = -1,
     String description = 'Mock subscription',
     String color = '#CCCCCC',
     bool isMuted = false,
