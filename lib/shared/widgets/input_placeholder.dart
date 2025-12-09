3bcc1ddebb670ebe0010094fed0beaacd5f62f98
Eugene Koretsky <E.Koretsky@outlook.com>
2025-12-09 16:06:30 +0300

    feat: add input placeholder

---
+++ lib/shared/widgets/input_placeholder.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:genesis_workspace/i18n/generated/strings.g.dart';
+
+class InputPlaceholder extends StatelessWidget {
+  const InputPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = TextTheme.of(context);
+    return SizedBox(
+      height: 60.0,
+      width: double.infinity,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: BoxBorder.fromLTRB(top: BorderSide(color: Colors.white10))
+        ),
+        child:  Center(
+          child:  Text(context.t.inputPlaceholder, style: textTheme.bodySmall!.copyWith(
+            fontSize: 16
+          ),),
+        ),
+      ),
+    );
+  }
+}
