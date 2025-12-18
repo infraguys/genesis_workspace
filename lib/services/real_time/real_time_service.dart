f2316077ff0f94ee06b5fce6b782399d2a01d7fc
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2025-12-18 17:33:06 +0300

    unread badges fix

--- lib/services/real_time/real_time_service.dart
+++ lib/services/real_time/real_time_service.dart
@@ -75,7 +75,6 @@ class RealTimeService {
             EventTypes.message,
             EventTypes.subscription,
             EventTypes.realm_user,
-            EventTypes.update_message_flags,
           ],
         ),
       );
