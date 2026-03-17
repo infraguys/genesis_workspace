5186dc70611a99d0b153fc07666ae2c0820cf5cc
Eugene Koretsky <E.Koretsky@outlook.com>
2026-03-17 18:41:01 +0300

    feat: update update_message_entity.dart and update_message_use_case.dart

--- lib/domain/messages/entities/update_message_entity.dart
+++ lib/domain/messages/entities/update_message_entity.dart
@@ -2,13 +2,29 @@ import 'package:genesis_workspace/data/messages/dto/update_message_dto.dart';
 import 'package:genesis_workspace/domain/common/entities/response_entity.dart';
 
 class UpdateMessageResponseEntity extends ResponseEntity {
-  UpdateMessageResponseEntity({required super.msg, required super.result});
+  UpdateMessageResponseEntity({
+    required super.msg,
+    required super.result,
+  });
 }
 
 class UpdateMessageRequestEntity {
-  final int messageId;
-  final String content;
-  UpdateMessageRequestEntity({required this.messageId, required this.content});
+  UpdateMessageRequestEntity({
+    required this.messageId,
+    this.content,
+    this.topic,
+    this.propagateMode,
+  });
 
-  UpdateMessageRequestDto toDto() => UpdateMessageRequestDto(messageId: messageId, content: content);
+  final int messageId;
+  final String? content;
+  final String? topic;
+  final String? propagateMode;
+
+  UpdateMessageRequestDto toDto() => UpdateMessageRequestDto(
+    messageId: messageId,
+    content: content,
+    topic: topic,
+    propagateMode: propagateMode,
+  );
 }
