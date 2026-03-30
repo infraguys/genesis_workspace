5337569c97e4fd063c59d040e65b604c2539bc88
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2026-03-30 15:19:03 +0300

    window buttons

--- lib/core/widgets/desktop_app_bar.dart
+++ lib/core/widgets/desktop_app_bar.dart
@@ -12,17 +12,53 @@ class DesktopAppBar extends StatelessWidget {
       child: SizedBox(
         height: 40,
         width: double.maxFinite,
-        child: MoveWindow(
-          onDoubleTap: () async {
-            final isMaximized = await windowManager.isMaximized();
-            if (isMaximized) {
-              windowManager.unmaximize();
-            } else {
-              windowManager.maximize();
-            }
-          },
+        child: Row(
+          children: [
+            Expanded(
+              child: MoveWindow(
+                onDoubleTap: () async {
+                  final isMaximized = await windowManager.isMaximized();
+                  if (isMaximized) {
+                    windowManager.unmaximize();
+                  } else {
+                    windowManager.maximize();
+                  }
+                },
+              ),
+            ),
+            WindowButtons(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+final buttonColors = WindowButtonColors(
+  iconNormal: const Color(0xFF805306),
+  mouseOver: const Color(0xFFF6A00C),
+  mouseDown: const Color(0xFF805306),
+  iconMouseOver: const Color(0xFF805306),
+  iconMouseDown: const Color(0xFFFFD500),
+);
+
+final closeButtonColors = WindowButtonColors(
+  mouseOver: const Color(0xFFD32F2F),
+  mouseDown: const Color(0xFFB71C1C),
+  iconNormal: const Color(0xFF805306),
+  iconMouseOver: Colors.white,
+);
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
     );
   }
 }
