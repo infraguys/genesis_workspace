b4f47f12c1e6c6a837acdffc3bd97add4b47081d
Ivan Starkov <starig@Ivans-MacBook-Pro.local>
2026-04-02 17:49:00 +0300

    refactor

--- lib/core/config/emoji_picker_config.dart
+++ lib/core/config/emoji_picker_config.dart
@@ -2,10 +2,9 @@ import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:emoji_picker_flutter/locales/default_emoji_set_locale.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis_workspace/core/utils/helpers.dart';
 import 'package:genesis_workspace/features/emoji_keyboard/bloc/emoji_keyboard_cubit.dart';
 import 'package:genesis_workspace/i18n/generated/strings.g.dart';
-
-String toEmojiCode(String emoji) => emoji.runes.map((r) => r.toRadixString(16)).join('-');
 
 List<CategoryEmoji> buildLimitedEmojiSet(
   Locale locale,
@@ -17,7 +16,7 @@ List<CategoryEmoji> buildLimitedEmojiSet(
       .map(
         (c) => CategoryEmoji(
           c.category,
-          c.emoji.where((e) => allowedCodes.contains(toEmojiCode(e.emoji))).toList(),
+          c.emoji.where((e) => allowedCodes.contains(emojiToCode(e.emoji))).toList(),
         ),
       )
       .where((c) => c.emoji.isNotEmpty)
